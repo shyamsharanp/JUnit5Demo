e9176fab72690472d57b64542d30450dba08be5f
nidhiprajapati0820 <nidhi.khurpia@gmail.com>
2022-01-09 08:12:31 -0700

    Initial commit

---
+++ src/main/java/com/simplilearn/JUnit5Demo/JUnit5DemoApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.JUnit5Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JUnit5DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JUnit5DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplilearn/JUnit5Demo/JUnit5DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.JUnit5Demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JUnit5DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
