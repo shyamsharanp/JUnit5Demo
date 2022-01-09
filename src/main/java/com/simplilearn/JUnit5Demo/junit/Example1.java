e22aa55b7e1c34038fff5cd164ef2c323b0a54a8
nidhiprajapati0820 <nidhi.khurpia@gmail.com>
2022-01-09 08:30:07 -0700

    basic test cases

---
+++ src/main/java/com/simplilearn/JUnit5Demo/junit/Example1.java
@@ -0,0 +1,49 @@
+package com.simplilearn.JUnit5Demo.junit;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.runner.RunWith;
+
+@SuppressWarnings("deprecation")
+@DisplayName("This is example1 test suit..")
+@RunWith(JUnitPlatform.class)
+public class Example1 {
+	
+	@BeforeAll
+	static void initAll() { //should be static if it is before all..
+		System.out.println("Before ALL excecutes..");
+	}
+	
+	@BeforeEach
+	void init() {
+		System.out.println("Before Each method executes..");
+	}
+
+	@Test
+	@DisplayName("This is success test.. display name.")
+	void successTest() {
+		System.out.println("Executing success test..");
+	}
+	
+	@Test
+	void successTest1() {
+		System.out.println("Executing success test1..");
+	}
+	
+	@AfterEach
+	void tearDown() {
+		System.out.println("After each method executes..");
+	}
+	
+	@AfterAll
+	static void tearDownAll() { //Should be static method if it is After all..
+		System.out.println("After ALL method executes..");
+	}
+	
+	
+}
